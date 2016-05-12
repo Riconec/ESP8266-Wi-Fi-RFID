0563884a45d1819d39376da76f29be19e1831b40
Riconec <rulo90@mail.ru>
2016-05-12 15:48:32 +0300

    Code template
    Added code

---
+++ Code/ESP8266_RFID/ESP8266_RFID.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ESP8266_RFID.ino
+ Created:	5/12/2016 3:47:53 PM
+ Author:	epp
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
